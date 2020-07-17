c5590e53d0475e86d7bf737a45b2f598bbbe2cb2
Floriana Zefi <florianagjzefi@gmail.com>
2020-07-17 15:54:03 +0200

    changed to google docstring

--- autoCR/datasets.py
+++ autoCR/datasets.py
@@ -6,9 +6,12 @@ import pandas as pd
 def load_uci_credit_card(return_X_y=False, as_frame=False):
     """Loads the UCI Credit Card Dataset.
 
-    :param return_X_y: If True, returns ``(data, target)`` instead of a dict object.
-    :param as_frame: give the pandas dataframe instead of X, y matrices (default=False).
-    :return: dict with data and target.
+    Args:
+        return_X_y:  If True, returns ``(data, target)`` instead of a dict object.
+        as_frame: give the pandas dataframe instead of X, y matrices (default=False).
+
+    Returns: data and target.
+
     """
     filepath = resource_filename("autoCR", os.path.join("data", "UCI_Credit_Card.zip"))
     df = pd.read_csv(filepath)
