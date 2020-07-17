60fee52e0116aa7c6a24b46738ed91080dcd4a88
Ryan Chaves <ryan.chaves@ing.com>
2020-07-17 12:41:09 +0200

    #1 Simplify setup; README inclusion was causing errors

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
-import os
-
 from setuptools import setup, find_packages
-
-
-def read(fname):
-    """Helper function to read filename from repo path."""
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="auto-cr",
     version="0.1",
     description="Automated Credit Risk Modeling, 2020",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
     author="RPAA",
     author_email="sandro.bjelogrlic@ing.com, ryan.chaves@ing.com, floriana.zefi@ing.com",
     license="ING Open Source",
