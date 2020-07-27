ec2292f13798e6d8564279be13e4cd4ab2e74bc8
Dan <daniel.timbrell@ing.com>
2020-07-27 16:18:33 +0200

    fix resource_filename

---
+++ skorecard/__init__.py
@@ -0,0 +1,6 @@
+"""Temporary __init__ file.
+
+Because skorecard dir currently empty but needs to contain something for pipeline to run.
+
+If this is deleted, the resource_filename function contained in datasets.py seems to break
+"""
