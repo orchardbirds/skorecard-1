7a822dbca69c49359484784cb20b818021e36332
WE56SC <sandro.bjelogrlic@ing.com>
2020-08-12 16:59:12 +0200

    #23 docsttring added

--- skorecard/linear_model.py
+++ skorecard/linear_model.py
@@ -4,24 +4,72 @@ import numpy as np
 
 
 class LogisticRegression(lm.LogisticRegression):
+    """Extended Logisitic Regression class."""
 
-    def __init__(self, penalty='l2', *, dual=False, tol=1e-4, C=1.0,
-                 fit_intercept=True, intercept_scaling=1, class_weight=None,
-                 random_state=None, solver='lbfgs', max_iter=100,
-                 multi_class='auto', verbose=0, warm_start=False, n_jobs=None,
-                 l1_ratio=None):
-        super().__init__(penalty=penalty, dual=dual, tol=tol, C= C,
-                 fit_intercept=fit_intercept, intercept_scaling=intercept_scaling, class_weight=class_weight,
-                 random_state=random_state, solver=solver, max_iter=max_iter,
-                 multi_class=multi_class, verbose=verbose, warm_start=warm_start, n_jobs=n_jobs,
-                 l1_ratio=l1_ratio
+    def __init__(
+        self,
+        penalty="l2",
+        *,
+        dual=False,
+        tol=1e-4,
+        C=1.0,
+        fit_intercept=True,
+        intercept_scaling=1,
+        class_weight=None,
+        random_state=None,
+        solver="lbfgs",
+        max_iter=100,
+        multi_class="auto",
+        verbose=0,
+        warm_start=False,
+        n_jobs=None,
+        l1_ratio=None
+    ):
+        """Extension of the Logistic Regression Class from sklearn."""
+        # __doc__ += super().__doc__
+
+        super().__init__(
+            penalty=penalty,
+            dual=dual,
+            tol=tol,
+            C=C,
+            fit_intercept=fit_intercept,
+            intercept_scaling=intercept_scaling,
+            class_weight=class_weight,
+            random_state=random_state,
+            solver=solver,
+            max_iter=max_iter,
+            multi_class=multi_class,
+            verbose=verbose,
+            warm_start=warm_start,
+            n_jobs=n_jobs,
+            l1_ratio=l1_ratio,
         )
 
         self.cov_matrix = None
 
+    def fit(self, X, y, sample_weight=None):
+        """Overwrittten function for the fit method of the sklearn Logistic Regression.
 
-    def fit(self, X,y,sample_weight=None):
-        super().fit(X,y,sample_weight=sample_weight)
+        In addition to the standard fit by sklearn, this function will compute the covariance of the coefficients.
+
+        Args:
+
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of samples and
+            n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+        sample_weight : array-like of shape (n_samples,) default=None
+            Array of weights that are assigned to individual samples.
+            If not provided, then each sample is given unit weight.
+            .. versionadded:: 0.17
+               *sample_weight* support to LogisticRegression.
+
+        Returns:
+            self, Fitted estimator.
+        """
+        super().fit(X, y, sample_weight=sample_weight)
 
         predProbs = self.predict_proba(X)
 
@@ -42,10 +90,10 @@ class LogisticRegression(lm.LogisticRegression):
         self.std_err_intercept_ = std_err[0]
         self.std_err_coef_ = std_err[1:]
 
-        self.z_intercept_ = self.intercept_/self.std_err_intercept_
-        self.z_coef_ = self.coef_/self.std_err_coef_
+        self.z_intercept_ = self.intercept_ / self.std_err_intercept_
+        self.z_coef_ = self.coef_ / self.std_err_coef_
 
-        self.p_val_intercept_ = scipy.stats.norm.sf(abs(self.z_intercept_))*2
-        self.p_val_coef_ = scipy.stats.norm.sf(abs(self.z_coef_))*2
+        self.p_val_intercept_ = scipy.stats.norm.sf(abs(self.z_intercept_)) * 2
+        self.p_val_coef_ = scipy.stats.norm.sf(abs(self.z_coef_)) * 2
 
         return self
