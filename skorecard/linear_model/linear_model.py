134a4d7abab15391db9c387ebaa34fafad1b81b1
WE56SC <sandro.bjelogrlic@ing.com>
2020-08-13 21:36:41 +0200

    #23 removed useless constructor

--- skorecard/linear_model/linear_model.py
+++ skorecard/linear_model/linear_model.py
@@ -4,52 +4,20 @@ import numpy as np
 
 
 class LogisticRegression(lm.LogisticRegression):
-    """Extended Logisitic Regression class."""
+    """Extends the  Logisitic Regression class implemented in sklearn.
 
-    def __init__(
-        self,
-        penalty="l2",
-        *,
-        dual=False,
-        tol=1e-4,
-        C=1.0,
-        fit_intercept=True,
-        intercept_scaling=1,
-        class_weight=None,
-        random_state=None,
-        solver="lbfgs",
-        max_iter=100,
-        multi_class="auto",
-        verbose=0,
-        warm_start=False,
-        n_jobs=None,
-        l1_ratio=None
-    ):
-        """Extension of the Logistic Regression Class from sklearn."""
-        # __doc__ += super().__doc__
-
-        super().__init__(
-            penalty=penalty,
-            dual=dual,
-            tol=tol,
-            C=C,
-            fit_intercept=fit_intercept,
-            intercept_scaling=intercept_scaling,
-            class_weight=class_weight,
-            random_state=random_state,
-            solver=solver,
-            max_iter=max_iter,
-            multi_class=multi_class,
-            verbose=verbose,
-            warm_start=warm_start,
-            n_jobs=n_jobs,
-            l1_ratio=l1_ratio,
-        )
-
-        self.cov_matrix = None
+    In addition to the regular fit, it computes the following useful statistics.
+    - cov_matrix_: covariance matrix for the estimated parameters.
+    - std_err_intercept_: estimated uncertainty for the intercept
+    - std_err_coef_: estimated uncertainty for the coefficients
+    - z_intercept_: estimated z-statistic for the intercept
+    - z_coef_: estimated z-statistic for the coefficients
+    - p_value_intercept_: estimated p-value for the intercept
+    - p_value_coef_: estimated p-value for the coefficients
+    """
 
     def fit(self, X, y, sample_weight=None):
-        """Overwrittten function for the fit method of the sklearn Logistic Regression.
+        """Overwritten function for the fit method of the sklearn Logistic Regression.
 
         In addition to the standard fit by sklearn, this function will compute the covariance of the coefficients.
 
@@ -67,30 +35,44 @@ class LogisticRegression(lm.LogisticRegression):
         Returns:
             self, Fitted estimator.
         """
-        super().fit(X, y, sample_weight=sample_weight)
+        lr = super().fit(X, y, sample_weight=sample_weight)
 
         predProbs = self.predict_proba(X)
 
         # Design matrix -- add column of 1's at the beginning of your X_train matrix
-        X_design = np.hstack([np.ones((X.shape[0], 1)), X])
+        if lr.fit_intercept:
+            X_design = np.hstack([np.ones((X.shape[0], 1)), X])
+        else:
+            X_design = X
 
         # Initiate matrix of 0's, fill diagonal with each predicted observation's variance
         V = np.diagflat(np.product(predProbs, axis=1))
 
         # Covariance matrix following the algepra
-        self.cov_matrix = np.linalg.inv(np.dot(np.dot(X_design.T, V), X_design))
+        self.cov_matrix_ = np.linalg.inv(np.dot(np.dot(X_design.T, V), X_design))
 
-        std_err = np.sqrt(np.diag(self.cov_matrix))
+        std_err = np.sqrt(np.diag(self.cov_matrix_))
 
         # Index 0 corresponds to the intercept, from 1 onwards
-        self.std_err_intercept_ = std_err[0]
-        self.std_err_coef_ = std_err[1:]
+        if lr.fit_intercept:
+            self.std_err_intercept_ = std_err[0]
+            self.std_err_coef_ = std_err[1:]
 
-        self.z_intercept_ = self.intercept_ / self.std_err_intercept_
+            self.z_intercept_ = self.intercept_ / self.std_err_intercept_
+
+            # Get p-values under the gaussian assumption
+            self.p_val_intercept_ = scipy.stats.norm.sf(abs(self.z_intercept_)) * 2
+
+        else:
+            self.std_err_intercept_ = np.nan
+            self.std_err_coef_ = std_err
+
+            self.z_intercept_ = np.nan
+
+            # Get p-values under the gaussian assumption
+            self.p_val_intercept_ = np.nan
+
         self.z_coef_ = self.coef_ / self.std_err_coef_
-
-        # Get p-values under the gaussian assumption
-        self.p_val_intercept_ = scipy.stats.norm.sf(abs(self.z_intercept_)) * 2
         self.p_val_coef_ = scipy.stats.norm.sf(abs(self.z_coef_)) * 2
 
         return self
