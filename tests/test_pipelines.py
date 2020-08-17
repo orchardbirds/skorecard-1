130372e652658bcf35c1ff52e221402ec76eadfa
WE56SC <sandro.bjelogrlic@ing.com>
2020-08-17 14:59:15 +0200

    #37 removed/adapted tests in pipeline designed to catch multi dimension behavior

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -5,6 +5,7 @@ from skorecard.preprocessing import SimpleBucketTransformer
 from skorecard.preprocessing import AgglomerativeBucketTransformer
 from skorecard.preprocessing import QuantileBucketTransformer
 import pytest
+from skorecard.utils.exceptions import DimensionalityError
 
 
 @pytest.fixture()
@@ -13,13 +14,28 @@ def df():
     return datasets.load_uci_credit_card(as_frame=True)
 
 
+def test_bucket_transformer_exception(df):
+    """Test the exception is raised in scikit-learn pipeline."""
+    transformer = ColumnTransformer(
+        transformers=[
+            ("simple", SimpleBucketTransformer(bin_count=2), [1]),
+            ("agglom", AgglomerativeBucketTransformer(bin_count=4), [0]),
+            ("quantile", QuantileBucketTransformer(bin_count=[10, 6]), [2, 3]),
+        ],
+        remainder="passthrough",
+    )
+    with pytest.raises(DimensionalityError):
+        transformer.fit_transform(df.values)
+
+
 def test_bucket_transformer(df):
     """Test that we can utilise the main bucket transformers in a scikit-learn pipeline."""
     transformer = ColumnTransformer(
         transformers=[
             ("simple", SimpleBucketTransformer(bin_count=2), [1]),
             ("agglom", AgglomerativeBucketTransformer(bin_count=4), [0]),
-            ("quantile", QuantileBucketTransformer(bin_count=[10, 6]), [2, 3]),
+            ("quantile_0", QuantileBucketTransformer(bin_count=[10]), [2]),
+            ("quantile_1", QuantileBucketTransformer(bin_count=[6]), [3]),
         ],
         remainder="passthrough",
     )
