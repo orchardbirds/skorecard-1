cf29f08034d04b1b9e0758ff9fb6389b35b3c853
WE56SC <sandro.bjelogrlic@ing.com>
2020-08-17 14:53:06 +0200

    #37 #35 removed/adapted tests in simple bucketing designed to catch multi dimension behavior

--- tests/test_simple_bucket_transformer.py
+++ tests/test_simple_bucket_transformer.py
@@ -1,5 +1,7 @@
 from skorecard import datasets
 from skorecard.preprocessing import SimpleBucketTransformer
+from skorecard.utils.exceptions import DimensionalityError
+
 
 import pytest
 
@@ -28,46 +30,19 @@ def test_bin_counts(df):
 
     # Test multiple bin counts
     SBT = SimpleBucketTransformer(bin_count=[7, 5])
-    SBT.fit_transform(df[["LIMIT_BAL", "BILL_AMT1"]].values)
-    assert SBT.BucketDict["Feature_0"].bin_count == 7
-    assert SBT.BucketDict["Feature_1"].bin_count == 5
-
-    return None
+    with pytest.raises(DimensionalityError):
+        SBT.fit_transform(df[["LIMIT_BAL", "BILL_AMT1"]].values)
 
 
 def test_float_bin_count(df):
     """Test that a float for bin_count raises an error."""
-    try:
+    with pytest.raises(AttributeError):
         SBT = SimpleBucketTransformer(bin_count=7.3)
         SBT.fit_transform(df["LIMIT_BAL"].values)
-    except AttributeError:
-        assert True
-
-    return None
 
 
 def test_bucket_dict_number(df):
-    """Test that we get a separate Bucketer object per number of columns given."""
+    """Test that we get an exception if the wrong bucketer is raised."""
     SBT = SimpleBucketTransformer(bin_count=[3, 3])
-    SBT.fit(df[["LIMIT_BAL", "BILL_AMT1"]].values)
-    assert len(SBT.BucketDict) == 2
-
-    return None
-
-
-def test_bad_bin_count_shape(df):
-    """Test that bad bin shape triggers ValueError."""
-    # Simple Bucketer
-    try:
-        SBT = SimpleBucketTransformer(bin_count=[3, 3])
-        SBT.fit(df["LIMIT_BAL"].values)
-    except ValueError:
-        assert True
-
-    try:
-        SBT = SimpleBucketTransformer(bin_count=[3])
+    with pytest.raises(DimensionalityError):
         SBT.fit(df[["LIMIT_BAL", "BILL_AMT1"]].values)
-    except ValueError:
-        assert True
-
-    return None
