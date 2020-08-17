1dfb61424778492481247323aae87f8c6c1157ec
WE56SC <sandro.bjelogrlic@ing.com>
2020-08-17 14:43:22 +0200

    #37 #35 removed tests in agglomertive bucketing designed to catch multi dimension behavior

--- tests/test_agglomerative_bucket_transformer.py
+++ tests/test_agglomerative_bucket_transformer.py
@@ -1,5 +1,6 @@
 from skorecard import datasets
 from skorecard.preprocessing import AgglomerativeBucketTransformer
+from skorecard.utils.exceptions import DimensionalityError
 
 import pytest
 
@@ -26,49 +27,19 @@ def test_bin_counts(df):
     ABT.fit_transform(df["MARRIAGE"].values)
     assert ABT.BucketDict["Feature_0"].bin_count == 3
 
+    return None
+
+
+def test_dimensionality_exception(df):
+    """Test the exception is raised if too many features are run at the same time."""
     # Test multiple bin counts
     ABT = AgglomerativeBucketTransformer(bin_count=[2, 2, 3])
-    ABT.fit_transform(df[["LIMIT_BAL", "MARRIAGE", "BILL_AMT1"]].values)
-    assert ABT.BucketDict["Feature_0"].bin_count == 2
-    assert ABT.BucketDict["Feature_1"].bin_count == 2
-    assert ABT.BucketDict["Feature_2"].bin_count == 3
-
-    return None
+    with pytest.raises(DimensionalityError):
+        ABT.fit_transform(df[["LIMIT_BAL", "MARRIAGE", "BILL_AMT1"]].values)
 
 
 def test_float_bin_count(df):
     """Test that a float for bin_count raises an error."""
-    try:
+    with pytest.raises(AttributeError):
         ABT = AgglomerativeBucketTransformer(bin_count=7.3)
         ABT.fit_transform(df["LIMIT_BAL"].values)
-    except AttributeError:
-        assert True
-
-    return None
-
-
-def test_bucket_dict_number(df):
-    """Test that we get a separate Bucketer object per number of columns given."""
-    ABT = AgglomerativeBucketTransformer(bin_count=[4, 2, 3])
-    ABT.fit(df[["MARRIAGE", "BILL_AMT1", "LIMIT_BAL"]].values)
-    assert len(ABT.BucketDict) == 3
-
-    return None
-
-
-def test_bad_bin_count_shape(df):
-    """Test that bad bin shape triggers ValueError."""
-    # Agglomerative Bucketer
-    try:
-        ABT = AgglomerativeBucketTransformer(bin_count=[3, 3])
-        ABT.fit(df["LIMIT_BAL"].values)
-    except ValueError:
-        assert True
-
-    try:
-        ABT = AgglomerativeBucketTransformer(bin_count=[3])
-        ABT.fit(df[["LIMIT_BAL", "BILL_AMT1"]].values)
-    except ValueError:
-        assert True
-
-    return None
