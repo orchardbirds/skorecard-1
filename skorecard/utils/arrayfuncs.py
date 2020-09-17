dd4f091f21cfd758bdd7a5679603d48cd4799cac
Sandro <sandro.bjelogrlic@ing.com>
2020-09-17 10:57:58 +0200

    2 fix the pipeline crash on docstrings

--- skorecard/utils/arrayfuncs.py
+++ skorecard/utils/arrayfuncs.py
@@ -7,42 +7,39 @@ import warnings
 
 
 def check_1d(x):
-    """
-    Checks whether or not a list, numpy array, pandas dataframe, pandas series are one-dimensional.
+    """Checks whether or not a list, numpy array, pandas dataframe, pandas series are one-dimensional.
+
     Returns True when check is ok, otherwise throws a `DimensionalityError`
 
     Args:
         x: list, numpy array, pandas dataframe, pandas series
 
     Returns: True or throws `DimensionalityError`
-
     """
     if isinstance(x, list):
         if any([isinstance(el, list) for el in x]):
-            raise DimensionalityError('The input is not 1D')
+            raise DimensionalityError("The input is not 1D")
         else:
             return True
     if isinstance(x, np.ndarray):
         if x.ndim == 1 and all([isinstance(el, numbers.Number) for el in x]):
             return True
         else:
-            raise DimensionalityError('The input is not 1D')
+            raise DimensionalityError("The input is not 1D")
     if isinstance(x, pd.core.frame.DataFrame):
-        if len(x.columns) == 1 and pd.api.types.is_numeric_dtype(
-                x[x.columns[0]]):
+        if len(x.columns) == 1 and pd.api.types.is_numeric_dtype(x[x.columns[0]]):
             return True
         else:
-            raise DimensionalityError('The input is not 1D')
+            raise DimensionalityError("The input is not 1D")
     if isinstance(x, pd.core.series.Series):
         if x.ndim == 1 and pd.api.types.is_numeric_dtype(x):
             return True
         else:
-            raise DimensionalityError('The input is not 1D')
+            raise DimensionalityError("The input is not 1D")
 
 
 def assure_numpy_array(x, assure_1d=False):
-    """
-    Returns x as numpy array. X can be a list, numpy array, pandas dataframe, pandas series.
+    """Returns x as numpy array. X can be a list, numpy array, pandas dataframe, pandas series.
 
     Args:
         x: list, numpy array, pandas dataframe, pandas series
@@ -66,8 +63,7 @@ def assure_numpy_array(x, assure_1d=False):
 
 
 def assure_pandas_df(x):
-    """
-    Returns x as pandas DataFrame. X can be a list, list of lists, numpy array, pandas DataFrame or pandas Series.
+    """Returns x as pandas DataFrame. X can be a list, list of lists, numpy array, pandas DataFrame or pandas Series.
 
     Args:
         x (list, numpy array, pandas DataFrame, pandas Series): array to be tested
@@ -77,28 +73,23 @@ def assure_pandas_df(x):
     """
     if isinstance(x, pd.core.frame.DataFrame):
         return x
-    elif any([
-        isinstance(x, np.ndarray),
-        isinstance(x, pd.core.series.Series),
-        isinstance(x, list)
-    ]):
+    elif any([isinstance(x, np.ndarray), isinstance(x, pd.core.series.Series), isinstance(x, list)]):
         return pd.DataFrame(x)
     else:
-        raise TypeError(
-            "Please supply a list, numpy array, pandas Series or pandas DataFrame"
-        )
+        raise TypeError("Please supply a list, numpy array, pandas Series or pandas DataFrame")
 
 
 def warn_if_missing(variable, variable_name):
-    """
-    Checks if for missing values: if there are notify the user.
+    """Checks if for missing values: if there are notify the user.
 
     Args:
         variable (pandas.DataFrame, pandas.Series or numpy.ndarray): data to be checked for missing values.
         variable_name (str): Name of the variable checked.
     """
-    warning_text = "You have missing values in your variable {}, this might cause the model to fail. Please either " \
-                   "impute the missing values or use a model that can handle them e.g. XGBoost.".format(variable_name)
+    warning_text = (
+        "You have missing values in your variable {}, this might cause the model to fail. Please either "
+        "impute the missing values or use a model that can handle them e.g. XGBoost.".format(variable_name)
+    )
 
     if isinstance(variable, (pd.DataFrame, pd.Series)):
         if variable.isnull().values.any():
@@ -109,8 +100,7 @@ def warn_if_missing(variable, variable_name):
 
 
 def check_numeric_dtypes(x):
-    """
-    Checks if all entries in an array are of a data type that can be interpreted as numeric (int, float or bool).
+    """Checks if all entries in an array are of a data type that can be interpreted as numeric (int, float or bool).
 
     Args:
         x (np.ndarray or pd.Series, list): array to be checked
@@ -128,4 +118,3 @@ def check_numeric_dtypes(x):
         if type(element.item()) not in allowed_types:
             raise TypeError("Please supply an array with only floats, ints or booleans")
     return x
-
