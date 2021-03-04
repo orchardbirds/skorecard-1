3f697a8c7d0833018db26512ae0988c92d62be99
Sandro <sandro.bjelogrlic@gmail.com>
2021-03-04 22:34:38 +0100

    tested bucketing process works well with pipelines

--- tests/test_bucketing_process.py
+++ tests/test_bucketing_process.py
@@ -1,6 +1,9 @@
 from skorecard.bucketers import OptimalBucketer, DecisionTreeBucketer
+from skorecard.preprocessing import WoeEncoder
 from skorecard.pipeline import BucketingProcess
 from skorecard.utils import NotPreBucketedError
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
 
 import pytest
 
@@ -40,3 +43,28 @@ def test_bucketing_optimization(df):
 
     for col in num_cols + cat_cols:
         assert X_bins[col].nunique() <= bucketing_process.X_prebucketed[col].nunique()
+
+
+def test_bucketing_process_in_pipeline(df):
+    """Test that it works fine withing a sklearn pipeline."""
+    num_cols = ["LIMIT_BAL", "BILL_AMT1"]
+    cat_cols = ["EDUCATION", "MARRIAGE"]
+
+    X = df[num_cols + cat_cols]
+    y = df["default"].values
+
+    bucketing_process = BucketingProcess()
+    bucketing_process.register_prebucketing_pipeline(
+        DecisionTreeBucketer(variables=num_cols, max_n_bins=100, min_bin_size=0.05)
+    )
+    bucketing_process.register_bucketing_pipeline(
+        OptimalBucketer(variables=num_cols, max_n_bins=10, min_bin_size=0.05),
+        OptimalBucketer(variables=cat_cols, max_n_bins=10, min_bin_size=0.05),
+    )
+
+    pipeline = make_pipeline(bucketing_process, WoeEncoder(), LogisticRegression())
+
+    pipeline.fit(X, y)
+    preds = pipeline.predict_proba(X)
+
+    assert preds.shape[0] == X.shape[0]
