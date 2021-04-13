a1c7d1392792a624671673606f5c44e147edd36c
Tim Vink <vinktim@gmail.com>
2021-04-13 16:17:54 +0200

    tweak image plot size and resolution

--- skorecard/reporting/plotting.py
+++ skorecard/reporting/plotting.py
@@ -15,6 +15,7 @@ try:
 except ModuleNotFoundError:
     Image = NotInstalledError("psutil")
 
+
 def plot_bins(X, y, col):
     """Plot bin table."""
     assert isinstance(X, pd.DataFrame)
@@ -38,7 +39,7 @@ def plot_bins(X, y, col):
     plotdf = plotdf.merge(er, how="left", on="bucket").sort_values("bucket")
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-    
+
     # Add traces
     fig.add_trace(
         go.Bar(x=plotdf["bucket"], y=plotdf["counts %"] * 100, name="Bucket count percentage"),
@@ -62,9 +63,10 @@ def plot_bins(X, y, col):
     )
     return fig
 
+
 def plot_prebucket_table(prebucket_table, X, y, column, format=None):
     """
-    Given the prebucketed data, plot the pre-buckets
+    Given the prebucketed data, plot the pre-buckets.
 
     Args:
         prebucket_table (pd.DataFrame): the table of the prebucketed data
@@ -77,7 +79,7 @@ def plot_prebucket_table(prebucket_table, X, y, column, format=None):
         fig of desired format
     """
     bucket_colors = get_bucket_colors() * 4  # We repeat the colors in case there are lots of buckets
-    buckets = [prebucket for prebucket in prebucket_table['pre-bucket'].values]
+    buckets = [prebucket for prebucket in prebucket_table["pre-bucket"].values]
     bar_colors = [bucket_colors[i] for i in buckets]
 
     fig = plot_bins(X, y, column)
@@ -88,13 +90,14 @@ def plot_prebucket_table(prebucket_table, X, y, column, format=None):
     fig.update_traces(marker=dict(color=bar_colors), selector=dict(type="bar"))
 
     if format is not None:
-        img_bytes = fig.to_image(format=format)
+        img_bytes = fig.to_image(format=format, scale=2, width=1050, height=525)
         fig = Image(img_bytes)
     return fig
 
+
 def plot_bucket_table(bucket_table, format=None):
     """
-    Given the bucketed data, plot the buckets with Event Rate
+    Given the bucketed data, plot the buckets with Event Rate.
 
     Args:
         bucket_table (pd.DataFrame): the table of the bucketed data
@@ -104,15 +107,15 @@ def plot_bucket_table(bucket_table, format=None):
         plotly fig
     """
     bucket_colors = get_bucket_colors() * 4  # We repeat the colors in case there are lots of buckets
-    buckets = [int(bucket) for bucket in bucket_table['bucket'].values]
+    buckets = [int(bucket) for bucket in bucket_table["bucket"].values]
     bar_colors = [bucket_colors[i] for i in buckets]
 
     plotdf = pd.DataFrame(
         {
             "bucket": buckets,
-            "counts": [int(count) for count in bucket_table['Count'].values],
-            "counts %": [float(count) for count in bucket_table['Count (%)'].values],
-            "Event Rate": [event for event in bucket_table['Event Rate'].values],
+            "counts": [int(count) for count in bucket_table["Count"].values],
+            "counts %": [float(count) for count in bucket_table["Count (%)"].values],
+            "Event Rate": [event for event in bucket_table["Event Rate"].values],
         }
     )
 
@@ -145,7 +148,7 @@ def plot_bucket_table(bucket_table, format=None):
     )
 
     if format is not None:
-        img_bytes = fig.to_image(format=format)
+        img_bytes = fig.to_image(format=format, scale=2, width=1050, height=525)
         fig = Image(img_bytes)
 
     return fig
