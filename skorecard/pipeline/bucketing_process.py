58c179aa69d0df2e3365ddd76ed848af6578e29c
Tim Vink <vinktim@gmail.com>
2021-04-14 15:46:20 +0200

    refactor bucketingprocess

--- skorecard/pipeline/bucketing_process.py
+++ skorecard/pipeline/bucketing_process.py
@@ -1,6 +1,5 @@
 from skorecard.utils import NotPreBucketedError, NotBucketObjectError
 from skorecard.pipeline import get_features_bucket_mapping
-from skorecard.apps.app_utils import determine_boundaries
 from skorecard.reporting import create_report, plot_bucket_table, plot_prebucket_table
 from skorecard.bucketers import UserInputBucketer
 
@@ -11,6 +10,7 @@ from sklearn.utils.validation import check_is_fitted
 import pandas as pd
 
 import warnings
+
 
 class BucketingProcess(BaseEstimator, TransformerMixin):
     """Class to concatenate a prebucketing and bucketing step.
@@ -40,19 +40,19 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
 
     bucketing_process.fit(X, y)
 
-
-    # bucketing_process.summary() # all vars, and # buckets
-    # bucketing_process.bucket_table("varname")
-    # bucketing_process.plot_bucket("varname")
-    # bucketing_process.prebucket_table("varname")
-    # bucketing_process.plot_prebucket("varname")
-
+    # Details
+    bucketing_process.summary() # all vars, and # buckets
+    bucketing_process.bucket_table("LIMIT_BAL")
+    bucketing_process.plot_bucket("LIMIT_BAL")
+    bucketing_process.prebucket_table("LIMIT_BAL")
+    bucketing_process.plot_prebucket("LIMIT_BAL")
     ```
 
     """
 
     def __init__(self, specials={}):
-        """Init the class.
+        """
+        Init the class.
 
         Args:
             specials: (nested) dictionary of special values that require their own binning.
@@ -65,14 +65,17 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
         """
         self.prebucketing_pipeline = None
         self._prebucketing_specials = specials
+        self._bucketing_specials = dict()  # will be determined later.
+        self.name = "bucketingprocess"  # to be able to identity the bucketingprocess in a pipeline
+
         self.specials = specials  # I have no idea why this is needed. Remove it for insane errors
-        self.name = "bucketingprocess"
 
     def _check_all_bucketers(self, steps):
-        """Checks all bucketing steps are skorecard bucketers.
+        """
+        Ensure all specified bucketing steps are skorecard bucketers.
 
         Args:
-            steps: skorecard bucketers
+            steps: steps in a scikitlearn pipeline
         """
         for step in steps:
             msg = "All bucketing steps must be skorecard bucketers"
@@ -87,7 +90,7 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
         the BucketingProcess ends up with the final buckets.
         An example is seen below:
 
-        pre-bucket | label              | Count | Count (%) | Non-event | Event | Event Rate | WoE |  IV   | bucket
+        bucket     | label              | Count | Count (%) | Non-event | Event | Event Rate | WoE |  IV   | bucket
         -----------------------------------------------------------------------------------------------------------
         0          | (-inf, 25000.0)    | 479.0 | 7.98      | 300.0     | 179.0 | 37.37      | 0.73 | 0.05 | 0
         1          | [25000.0, 45000.0) | 370.0 | 6.17      | 233.0     | 137.0 | 37.03      | 0.71 | 0.04 | 1
@@ -99,26 +102,10 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
             df (pd.DataFrame): A pandas dataframe of the format above
         """  # noqa
         check_is_fitted(self)
-        if column not in self.X.columns:
-            raise ValueError(f"column {column} not in columns of X {self.X.columns}")
+        if column not in self.bucket_tables_.keys():
+            raise ValueError(f"column '{column}' was not part of the bucketingprocess")
 
-        prebucket_table = self.prebucket_table(column=column)
-        new_buckets = pd.DataFrame()
-        new_buckets["pre_buckets"] = [prebucket for prebucket in prebucket_table["pre-bucket"].values]
-        new_buckets["buckets"] = [int(bucket) for bucket in prebucket_table["bucket"].values]
-
-        bucket_mapping = self.ui_bucketer.features_bucket_mapping.get(column)
-
-        boundaries = determine_boundaries(new_buckets, bucket_mapping)
-        self.ui_bucketer.features_bucket_mapping.get(column).map = boundaries
-
-        table = create_report(
-            self.X_prebucketed_,
-            self.y,
-            column=column,
-            bucket_mapping=self.ui_bucketer.features_bucket_mapping.get(column),
-            display_missing=False,
-        )
+        table = self.bucket_tables_.get(column)
         table = table.rename(columns={"bucket_id": "bucket"})
         table["Event Rate"] = round(table["Event Rate"] * 100, 2)
         return table
@@ -141,18 +128,16 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
             df (pd.DataFrame): A pandas dataframe of the format above
         """  # noqa
         check_is_fitted(self)
-        if column not in self.X.columns:
-            raise ValueError(f"column {column} not in columns of X {self.X.columns}")
+        if column not in self.prebucket_tables_.keys():
+            raise ValueError(f"column '{column}' was not part of the pre-bucketing process")
 
-        table = create_report(
-            self.X, self.y, column=column, bucket_mapping=self._features_prebucket_mapping.get(column)
-        )
+        table = self.prebucket_tables_.get(column)
 
         table["Event Rate"] = round(table["Event Rate"] * 100, 2)
         table = table.rename(columns={"bucket_id": "pre-bucket"})
 
         # Apply bucket mapping
-        bucket_mapping = self.ui_bucketer.features_bucket_mapping.get(column)
+        bucket_mapping = self._features_bucket_mapping.get(column)
         table["bucket"] = bucket_mapping.transform(table["pre-bucket"])
         return table
 
@@ -200,8 +185,14 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
                 enforce_all_bucketers: Make sure all steps are skorecard bucketers
         """
         self._check_all_bucketers(steps)
+        # Add specials to all bucketers
+        for step in steps:
+            if type(step) != tuple:
+                step.specials = self._prebucketing_specials
+            else:
+                step[1].specials = self._prebucketing_specials
+
         self.prebucketing_pipeline = make_pipeline(*steps, **kwargs)
-        self._remap_specials_pipeline(level="prebucketing")
 
     def register_bucketing_pipeline(self, *steps, **kwargs):
         """
@@ -230,58 +221,66 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
             X (pd.DataFrame): [description]
             y ([type], optional): [description]. Defaults to None.
         """
-        self.X = X
-        self.y = y
-        self.X_prebucketed_ = self.prebucketing_pipeline.fit_transform(X, y)
-
-        # find the prebucket features bucket mapping. This is necessary
-        # to find the mappings of the specials for the bucketing step.
+        # Fit the prebucketing pipeline
+        # And save the prebucket mapping
+        X_prebucketed_ = self.prebucketing_pipeline.fit_transform(X, y)
+        assert isinstance(X_prebucketed_, pd.DataFrame)
         self._features_prebucket_mapping = get_features_bucket_mapping(self.prebucketing_pipeline)
+        # and calculate the prebucket tables.
+        self.prebucket_tables_ = dict()
+        for column in X.columns:
+            if column in self._features_prebucket_mapping.maps.keys():
+                self.prebucket_tables_[column] = create_report(
+                    X, y, column=column, bucket_mapping=self._features_prebucket_mapping.get(column)
+                )
 
-        # define
+        # Find the new bucket numbers of the specials after prebucketing,
+        # and set self._bucketing_specials
         self._retrieve_special_for_bucketing()
+        # Then assign the new specials to all bucketers in the bucketing pipeline
+        for step in self.bucketing_pipeline.steps:
+            if type(step) != tuple:
+                step.specials = self._bucketing_specials
+            else:
+                step[1].specials = self._bucketing_specials
 
-        self._remap_specials_pipeline(level="bucketing")
-
-        self.bucketing_pipeline.fit(self.X_prebucketed_, y)
+        # Fit the prebucketing pipeline
+        # And save the bucket mapping
+        self.bucketing_pipeline.fit(X_prebucketed_, y)
         self._features_bucket_mapping = get_features_bucket_mapping(self.bucketing_pipeline)
-
-        # Add UI bucketer for report
-        self.ui_bucketer = UserInputBucketer(self._features_bucket_mapping)
-        self.pipeline = make_pipeline(self.prebucketing_pipeline, self.ui_bucketer, self.bucketing_pipeline)
+        # and calculate the bucket tables.
+        self.bucket_tables_ = dict()
+        for column in X.columns:
+            if column in self._features_bucket_mapping.maps.keys():
+                self.bucket_tables_[column] = create_report(
+                    X_prebucketed_, y, column=column, bucket_mapping=self._features_bucket_mapping.get(column)
+                )
 
         return self
 
-    def _remap_specials_pipeline(self, level="prebucketing"):
+    def _set_bucket_mapping(self, features_bucket_mapping, X_prebucketed, y):
         """
-        Add the specials in the prebucketing pipeline.
+        Replace the bucket mapping in the bucketing_pipeline.
 
-        Specials are designed to be defined in every bucketer.
-        This class passes it in the constructor.
-        Therefore, this needs to be remapped to the bucketers in the steps of the pipeline.
+        This is meant for use internally in the dash app, where we manually edit
+        bucketingprocess._features_bucket_mapping.
 
-        Args:
-            level (str, optional): define level at which to map Defaults to "prebucketing".
-
-        Raises:
-            ValueError: error raised in case the level argument is not supported.
+        To be able to update the bucketingprocess, use something like:
+        X_prebucketed = bucketingprocess.prebucket_pipeline.transform(X)
+        feature_bucket_mapping # your edited bucketingprocess._features_bucket_mapping
+        bucketingprocess._set_bucket_mapping(feature_bucket_mapping, X_prebucketed, y)
         """
-        if level == "prebucketing":
-            steps = self.prebucketing_pipeline.steps
-            specials = self._prebucketing_specials
-        elif level == "bucketing":
-            steps = self.bucketing_pipeline.steps
-            specials = self._bucketing_specials
-        else:
-            raise ValueError("level must be prebucketing or bucketing")
-        # map the specials to the prebucketer
-        for step in steps:
-            # Assign the special variables to all the bucketers
-            # in all the steps. This is not ideal, but it does not
-            # matter, because the bucketers ignore the special variables
-            # if not there.
-            # TODO: test this!
-            step[1].specials = specials
+        # Step 1: replace the bucketing pipeline with a UI bucketer that uses the new mapping
+        self.bucketing_pipeline = UserInputBucketer(features_bucket_mapping)
+        self._features_bucket_mapping = features_bucket_mapping
+
+        # Step 2: Recalculate the bucket tables
+        self.bucket_tables_ = dict()
+        for column in X_prebucketed.columns:
+            if column in self._features_bucket_mapping.maps.keys():
+                self.bucket_tables_[column] = create_report(
+                    X_prebucketed, y, column=column, bucket_mapping=self._features_bucket_mapping.get(column)
+                )
 
     def _retrieve_special_for_bucketing(self):
         """
@@ -291,8 +290,6 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
         for the bucketing step to the respective index from the prebucketing
         step.
         """
-        self._bucketing_specials = dict()
-
         for var in self._prebucketing_specials.keys():
             feats_preb_map = self._features_prebucket_mapping.maps[var]
             # this finds the maximum index within the
@@ -324,10 +321,8 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
     def transform(self, X):
         """Transform X through the prebucketing and bucketing pipelines."""
         check_is_fitted(self)
-        self.X_prebucketed = self.prebucketing_pipeline.transform(X)
-        self.X_bucketed = self.bucketing_pipeline.transform(self.X_prebucketed)
-
-        return self.X_bucketed
+        X_prebucketed = self.prebucketing_pipeline.transform(X)
+        return self.bucketing_pipeline.transform(X_prebucketed)
 
     def summary(self):
         """
@@ -349,15 +344,15 @@ class BucketingProcess(BaseEstimator, TransformerMixin):
             columns.append(col)
             # In case the column was never (pre)-bucketed
             try:
-                prebucket_number = len(self.prebucket_table(col)['pre-bucket'].unique())
+                prebucket_number = len(self.prebucket_table(col)["pre-bucket"].unique())
             except KeyError:
-                warnings.warn(f'Column {col} not pre-bucketed')
-                prebucket_number = 'not_bucketed'
+                warnings.warn(f"Column {col} not pre-bucketed")
+                prebucket_number = "not_bucketed"
             try:
-                bucket_number = len(self.bucket_table(col)['bucket'].unique())
+                bucket_number = len(self.bucket_table(col)["bucket"].unique())
             except KeyError:
-                warnings.warn(f'Column {col} not bucketed')
-                bucket_number = 'not_bucketed'
+                warnings.warn(f"Column {col} not bucketed")
+                bucket_number = "not_bucketed"
             num_prebuckets.append(prebucket_number)
             num_buckets.append(bucket_number)
             dtypes.append(self.X[col].dtype)
